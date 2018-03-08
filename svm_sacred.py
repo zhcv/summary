11f7278116b31b3972b57c014198b14bf5235562
zhpai <zhp9005@126.com>
2018-03-08 17:21:16 +0800

    add dcm info

---
+++ svm_sacred.py
@@ -0,0 +1,24 @@
+from numpy.random import permutation
+from sklearn import svm, datasets
+from sacred import Experiment
+ex = Experiment("iris_rbf_svm")
+
+
+
+@ex.config
+def cfg():
+    C = 1.0
+    gamma = 0.7
+
+
+@ex.automain
+def run(C, gamma):
+    iris = datasets.load_iris()
+    perm = permutation(iris.target.size)
+    iris.data = iris.data[perm]
+    iris.target = iris.target[perm]
+    clf = svm.SVC(C, 'rbf', gamma=gamma)
+    clf.fit(iris.data[:90],
+            iris.target[:90])
+    return clf.score(iris.data[90:],
+                iris.target[90:])
